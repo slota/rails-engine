99781ffb0fef63c684c8aa62792ceb93a9cbc84d
John Slota <john.slota@gmail.com>
2016-01-05 15:02:52 -0700

    relationship endpoints installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,46 +1,78 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :customers, default: { format: :json } do
+      resources :customers, only: [:index, :show], default: { format: :json } do
         collection do
           get "find"
           get "find_all"
           get "random"
         end
+
+        member do
+          get "invoices"
+          get "transactions"
+        end
       end
-      resources :invoice_items, default: { format: :json } do
+      resources :invoice_items, only: [:index, :show], default: { format: :json } do
         collection do
           get "find"
           get "find_all"
           get "random"
         end
+
+        member do
+          get "invoice"
+          get "item"
+        end
       end
-      resources :invoices, default: { format: :json } do
+      resources :invoices, only: [:index, :show], default: { format: :json } do
         collection do
           get "find"
           get "find_all"
           get "random"
         end
+
+        member do
+          get "transactions"
+          get "invoice_items"
+          get "items"
+          get "customer"
+          get "merchant"
+        end
       end
-      resources :items, default: { format: :json } do
+      resources :items, only: [:index, :show], default: { format: :json } do
         collection do
           get "find"
           get "find_all"
           get "random"
         end
+
+        member do
+          get "invoice_items"
+          get "merchants"
+        end
       end
-      resources :merchants, default: { format: :json } do
+      resources :merchants, only: [:index, :show], default: { format: :json } do
         collection do
           get "find"
           get "find_all"
           get "random"
         end
+
+        member do
+          get "items"
+          get "invoices"
+        end
       end
-      resources :transactions, default: { format: :json } do
+      resources :transactions, only: [:index, :show], default: { format: :json } do
         collection do
           get "find"
           get "find_all"
           get "random"
+        end
+
+        member do
+          get "invoice"
         end
       end
     end
